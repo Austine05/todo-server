7f95fe2bb3820a017998cad51b99e66879c3aa71
Austine05 <mraustineterry@gmail.com>
2023-06-28 13:22:17 +0100

    minor update on some comments

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Todo struct, this represents a TODO item
+// this represents a TODO item
 type Todo struct {
 	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
